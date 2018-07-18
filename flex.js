7df2eeb060549a2b23e6d9aa0016f87878d77a0c
Ben <ben.challenor@btopenworld.com>
2018-07-18 16:02:11 +0100

    Add toggleActive function to transition text

--- flex.js
+++ flex.js
@@ -4,5 +4,14 @@ function toggleOpen() {
   this.classList.toggle('open');
 }
 
+function toggleActive(e) {
+  // console.log(e.propertyName);
+  if (e.propertyName.includes('flex')) {
+    this.classList.toggle('open-active');
+  }
+}
+
 panels.forEach(panel => panel.addEventListener('click', toggleOpen));
 // listens to click on each panel then runs toggleOpen function
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+// once CSS has transitionend runs toggleActive
